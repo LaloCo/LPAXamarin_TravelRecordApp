975ed816a7fcabc394eafc7e0d66422923d4ad44
Eduardo Rosas <eduardorosas@MacBook-Pro-de-Eduardo.local>
2018-11-07 08:01:50 -0600

    Project as of lecture on showing user location

---
+++ TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MapPage : ContentPage
+	{
+		public MapPage ()
+		{
+			InitializeComponent ();
+
+            GetPermissions();
+		}
+
+        private async void GetPermissions()
+        {
+            try
+            {
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.LocationWhenInUse);
+                if (status != PermissionStatus.Granted)
+                {
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.LocationWhenInUse))
+                    {
+                        await DisplayAlert("Need your location", "We need to access your location", "Ok");
+                    }
+
+                    var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.LocationWhenInUse);
+                    if (results.ContainsKey(Permission.LocationWhenInUse))
+                        status = results[Permission.LocationWhenInUse];
+                }
+
+                if (status == PermissionStatus.Granted)
+                {
+                    locationsMap.IsShowingUser = true;
+                }
+                else
+                {
+                    await DisplayAlert("Location denied", "You didn't give us permission to access location, so we can't show you where you are", "Ok");
+                }
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");
+            }
+        }
+	}
+}
